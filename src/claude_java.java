01c7450e1f6924993377a532cead3ecfc0b6bd80
Your Name <dusingizimanajeanclaude@gmail.com>
2019-04-05 15:04:23 +0200

    first commit

---
+++ src/claude_java.java
@@ -0,0 +1,16 @@
+/**
+ * AWT Sample application
+ *
+ * @author 
+ * @version 1.00 19/03/29
+ */
+public class claude_java {
+    
+    public static void main(String[] args) {
+        // Create application frame.
+        claude_javaFrame frame = new claude_javaFrame();
+        
+        // Show frame
+        frame.setVisible(true);
+    }
+}
